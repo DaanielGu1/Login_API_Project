6f5a3741e58b61f55886a5a0137279f9f5bc5cba
DaanielGu1 <guim.oliveira.daniel@gmail.com>
2026-09-15 13:36:09 -0300

    refactor(project): refactored "main" function and "CreateDatabase" function
    Moved the "main" function to "main.go" file, moved the hard code query "table_query" to file "SQL/create_tables.sql" and read this file content in memory.

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,31 +1,4 @@
-package main
-
-import (
-	"database/sql"
-	"log"
-
-	_ "github.com/mattn/go-sqlite3"
-)
-
-var db *sql.DB
-
-func main() {
-
-	var ERROR error
-
-	db, ERROR = sql.Open("sqlite3", "database.db?_foreign_keys=on")
-	if ERROR != nil {
-		log.Fatalf("Error trying to access the database: %v", ERROR)
-	}
-	defer db.Close()
-
-	ERROR = db.Ping()
-	if ERROR != nil {
-		log.Fatalf("Error trying to interact with the database: %v", ERROR)
-	}
-	//"DEFAULT CURRENT_TIMESTAMP" may cause divergence depending on the user's computer clock
-	table_query := `
-	CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
 		username CHAR(50) UNIQUE,
 		first_name CHAR(50) NOT NULL,
@@ -50,9 +23,4 @@ func main() {
 		group_id INTEGER NOT NULL,
 		FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 		FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
-	);`
-	_, ERROR = db.Exec(table_query)
-	if ERROR != nil {
-		log.Fatalf("Error trying to build the database tables: %v", ERROR)
-	}
-}
+	);
---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/DaanielGu1/Login_API_Project/database"
+
+func main() {
+	database.CreateDatabase()
+
+}
